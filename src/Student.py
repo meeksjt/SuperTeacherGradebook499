57f6c74729185af1e33333f20dc9c0ba93ff9008
Kelcin <greekgeek8@gmail.com>
2018-04-06 14:37:24 -0500

    Finished Student

--- src/Student.py
+++ src/Student.py
@@ -1,6 +1,7 @@
 import sqlite3
 import copy
 from GlobalVariables import connection, cursor
+import uuid
 #Status: Know how to INSERT and CREATE. Learn how to UPDATE, then how to
 # SELECT and convert to an object.
 
@@ -12,21 +13,51 @@ class StudentList:
 		#This holds the name of the course
 		self.course = courseUUID
 		self.tableName = courseUUID+"_student_list"
-		cursor.execute("CREATE TABLE IF NOT EXISTS `"+self.tableName+"` (`uid`	TEXT,`ID`	INTEGER,`name`	TEXT,`email`	TEXT);")
+		cursor.execute("CREATE TABLE IF NOT EXISTS `"+self.tableName+"` (`uuid`	TEXT,`ID`	TEXT,`name`	TEXT,`email`	TEXT);")
 		connection.commit()
 		self.load_students()
 
 
-	def set_email(self,id,email):
+	def set_email(self,uuid,email):
 		for student in self.students:
-			if student.id == id:
+			if student.uuid == uuid:
 				print("Found one!")
-				student.setEmail(email)
+				student.set_email(email)
 				self.load_students()
 
+	def set_name(self,uuid,name):
+		for student in self.students:
+			if student.uuid == uuid:
+				print("Found one!")
+				student.set_name(name)
+				self.load_students()
+
+	def set_id(self,uuid,id):
+		for student in self.students:
+			if student.uuid == uuid:
+				print("Found one!")
+				student.set_id(id)
+				self.load_students()
+
+	def get_name(self,uuid):
+		for student in self.students:
+			if student.uuid == uuid:
+				return student.get_name()
+
+	def get_email(self,uuid):
+		for student in self.students:
+			if student.uuid == uuid:
+				return student.get_email()
+
+	def get_id(self,uuid):
+		for student in self.students:
+			if student.uuid == uuid:
+				return student.get_id()
+
 	def add_student(self,id,name,email):
-		newStudent = Student(id, id,name,email)
-		connection.execute(("INSERT INTO " + str(self.tableName) + " VALUES(" + str(newStudent.uuid) + "" + str(newStudent.id) + ", '" + str(newStudent.name) + "', '" + str(newStudent.email) + "')"))
+		newStudent = Student(self.tableName,id,name,email, uuid.uuid4())
+		#connection.execute(("INSERT INTO `cs499_student_list`(`uid`,`ID`,`name`,`email`) VALUES (`"+str(newStudent.uuid)+"`,"+str(newStudent.id)+",`"+str(newStudent.name+"`,`"+str(newStudent.email)+"`);")))
+		connection.execute(("INSERT INTO '" + str(self.tableName) + "' VALUES('" + str(newStudent.uuid) + "','" + str(newStudent.id) + "', '" + str(newStudent.name) + "', '" + str(newStudent.email) + "')"))
 		connection.commit()
 		self.__add_student(newStudent)
 
@@ -37,9 +68,9 @@ class StudentList:
 		for student in self.students:
 			student.save_student()
 
-	def remove_student(self,id):
+	def remove_student(self,uuid):
 		for student in self.students:
-			if student.id == id:
+			if student.uuid == uuid:
 				student.remove_student()
 				self.load_students()
 
@@ -48,7 +79,9 @@ class StudentList:
 		cursor.execute("SELECT * FROM `" + self.tableName + "`")
 		results = cursor.fetchall()
 		for row in results:
-			newStudent = Student(self.tableName, row[0], row[1], row[2])
+			print("Here is the row:", row)
+			#'4b9a8f74-3dd4-4cc8-b5fa-7f181c1b866a', 42, 'Jacob Houck', 'YourMom@Gmail.com'
+			newStudent = Student(self.tableName, row[1], row[2], row[3], row[0])
 			self.students.append(copy.deepcopy(newStudent))
 
 	def print_students(self):
@@ -57,16 +90,28 @@ class StudentList:
 
 class Student:
 #Need to reload students after setting name.
-	def __init__(self,tableName,id,name,email):
+	def __init__(self,tableName,id,name,email, uuid):
+		#'4b9a8f74-3dd4-4cc8-b5fa-7f181c1b866a', 42, 'Jacob Houck', 'YourMom@Gmail.com'
 		self.tableName = tableName
 		self.name = name
 		self.email = email
 		self.id = id
+		self.uuid = uuid
 
 	#connection.execute("INSERT INTO cs499_studentList VALUES(1, 'Jacob Houck', 'jeh0029@uah.edu')")
+
+	def get_email(self):
+		return self.email
+
+	def get_id(self):
+		return self.id
+
+	def get_name(self):
+		return self.name
+
 	def set_email(self,email):
 		self.email = email
-		query = "UPDATE "+self.tableName+" SET email = '" + str(self.email) + "' WHERE id = '" + str(self.id) + "';"
+		query = "UPDATE "+self.tableName+" SET email = '" + str(self.email) + "' WHERE uuid = '" + str(self.uuid) + "';"
 		print(query)
 		cursor.execute(query)
 		connection.commit()
@@ -75,7 +120,7 @@ class Student:
 		"""Tested"""
 		self.name=name
 		# I used a query to make it easier by creating our string, and just passing it to the cursor.
-		query = "UPDATE "+self.tableName+" SET name = '" + str(self.name) + "' WHERE id = " + str(self.id) + ";"
+		query = "UPDATE "+self.tableName+" SET name = '" + str(self.name) + "' WHERE uuid = '" + str(self.uuid) + "';"
 		print(query)
 		cursor.execute(query)
 		connection.commit()
@@ -84,7 +129,7 @@ class Student:
 		"""Sets ID in object and in database"""
 		self.id=id
 
-		query = "UPDATE "+self.tableName+" SET id = '" + str(self.id) + "' WHERE name = '" + str(self.name) + "';"
+		query = "UPDATE "+self.tableName+" SET id = '" + str(self.id) + "' WHERE uuid = '" + str(self.uuid) + "';"
 		print(query)
 		cursor.execute(query)
 		connection.commit()
@@ -99,10 +144,11 @@ class Student:
 	def print_student(self):
 		print("\nName: ", self.name)
 		print("ID: ", self.id)
+		print("UUID: ",self.uuid)
 		print("Email: ", self.email)
 
 	def remove_student(self):
-		query = "DELETE FROM "+self.tableName+" WHERE id = '" + str(self.id) + "';"
+		query = "DELETE FROM "+self.tableName+" WHERE uuid = '" + str(self.id) + "';"
 		print(query)
 		cursor.execute(query)
 		connection.commit()
@@ -121,7 +167,13 @@ def create_test_database():
 #Remember to find memes for the presentation. Be sure to get a HP one.
 
 students = StudentList("cs499")
-students.add_student("1", "Tyler Meeks", "jtm0036@uah.edu")
+#students.add_student("42","Jacob Houck", "YourMom@Gmail.com")
+#students.set_email("711bc1b8-265f-4236-a6b2-395bd2107879","HOLYMOLY@BATMAN.CSU")
+students.set_id("711bc1b8-265f-4236-a6b2-395bd2107879","LET THE BODIES HIT THE FLOOR!")
+students.set_name("711bc1b8-265f-4236-a6b2-395bd2107879","STAN")
+print("Hello?!?!")
+print(students.get_id("711bc1b8-265f-4236-a6b2-395bd2107879"), students.get_email("711bc1b8-265f-4236-a6b2-395bd2107879"),students.get_name("711bc1b8-265f-4236-a6b2-395bd2107879"))
+#students.print_students()
 #students.addStudent(id,name,email)
 #students.removeStudent(id)
 #students.students # This is an list you can iterate through, which has sets for email, id, and name.
