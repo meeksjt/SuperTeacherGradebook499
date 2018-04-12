ac37467eaf4eb76354a8355e91106210e09ef51d
meeksjt <jtm0036@uah.edu>
2018-04-11 19:28:43 -0500

    Modify Course and CourseManager

--- src/Course.py
+++ src/Course.py
@@ -1,4 +1,4 @@
-# Come back to this one
+# Delete Course
 import re
 import Assignment
 from AssignmentCategoryDict import AssignmentCategoryDict
@@ -9,7 +9,7 @@ from Attendance import AttendanceDictionary
 
 class Course(object):
 
-    def __init__(self, name="", number="", section="", semester="", course_uuid="invalid"):
+    def __init__(self, course_uuid="invalid", name="", number="", section="", semester=""):
         self.name = name
         self.number = number
         self.section = section
@@ -22,25 +22,20 @@ class Course(object):
 
         self.student_list = None
         self.grade_scale = None
-        self.assignment_category_list = None
+        self.assignment_category_dict = None
         self.attendance_dictionary = None
 
     def link_with_database(self):
         self.student_list = StudentList(self.course_uuid)
         self.grade_scale = GradeScale(self.course_uuid)
-        self.assignment_category_list = AssignmentCategoryDict(self.course_uuid, self.student_list)
+        self.assignment_category_dict = AssignmentCategoryDict(self.course_uuid, self.student_list)
         self.attendance_dictionary = AttendanceDictionary(self.course_uuid, self.student_list)
 
     def add_student(self, student_uuid, student_id, name, email):
         self.student_list.add_student(student_uuid, student_id, name, email)
-        pass
 
     def remove_student(self, uuid):
         self.student_list.remove_student(uuid)
-
-    # What is this function for?
-    def display_category(self):
-        pass
 
     def set_grade_scale(self, a, b, c, d):
         self.grade_scale.set_grade_scale(a, b, c, d)
